e51f454a709b1b5676b822ad91d99e8178b30f2a
x36 <ggalliosha@gmail.com>
2022-08-22 15:44:01 +0300

    Implemented CLI config storage

--- weather.js
+++ weather.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
+import { saveKeyValue } from './services/storage.service.js';
 import { printHelp, printError, printSuccess } from './services/log.service.js';
+
+const saveToken = async (token) => {
+    try {
+        await saveKeyValue('token', token);
+        printSuccess('Token saved!');
+    } catch (error) {
+        printError(error.message);
+    }
+};
 
 const initCli = () => {
     const args = getArgs(process.argv);
@@ -10,11 +20,12 @@ const initCli = () => {
     }
 
     if (args?.s) {
+        saveKeyValue('city', args.s);
         printSuccess(args.s);
     }
 
     if (args?.t) {
-        printSuccess(args.t);
+        return saveToken(args.t);
     }
 };
 
